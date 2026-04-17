a56c7b7927d940297274132ad934793d4e9051f2
Maxim <maksimuntyan@gmail.com>
2026-04-18 01:32:09 +0300

    jytj
    Remove commented-out code and improve clarity.

--- src/entities/config/BaseDao.ts
+++ src/entities/config/BaseDao.ts
@@ -4,12 +4,8 @@ import Config from "./Config";
 export default class BaseDao {
     static request = (url: string, init?: RequestInit) => {
         return new Promise<Response>((resolve, reject) => {
-          // реалізуємо свій формат api://... який буде адресуватись до бекенду
           if(url.startsWith("api://")) {
             url = url.replace("api://", Config.backendUrl + "/api/");
-            // console.log("BaseDao: GlobalState.token = ", GlobalState.token)
-            // перевіряємо стан авторизації та додаємо до запиту токен, якщо 
-            // він не встановлений ззовні
             if(GlobalState.token) {
               if(!init) {
                 init = {};
